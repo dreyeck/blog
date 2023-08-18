303d9158b6ee56ed4bad71bf9f6ba4a9ef84f9ab
RalfBarkow <ralf.barkow@me.com>
2023-08-18 11:27:04 +0200

    change default port
    On our server we get an error message: listen EADDRINUSE: address already in use. The reason is that our wiki server is configured to be available on port 3000. Therefore we have to use another port, e.g. 4040.

--- elm-pages-starter-express/adapters/express/server.mjs
+++ elm-pages-starter-express/adapters/express/server.mjs
@@ -2,7 +2,7 @@ import express from "express";
 import elmPagesMiddleware from "./middleware.mjs";
 
 const app = express();
-const port = 3000;
+const port = 4040;
 
 app.use(express.static("dist"));
 app.use(elmPagesMiddleware);
