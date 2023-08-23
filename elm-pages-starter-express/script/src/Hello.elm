e63f21c233a05c317246035d5d565522f626e86f
RalfBarkow <ralf.barkow@me.com>
2023-08-23 14:50:49 +0200

    add script/src/Hello.elm: Write Our Script Module
    https://wiki.ralfbarkow.ch/view/write-our-script-module/view/write-our-script-module_rev18

---
+++ elm-pages-starter-express/script/src/Hello.elm
@@ -0,0 +1,8 @@
+module Hello exposing (run)
+
+import Pages.Script as Script exposing (Script)
+
+run : Script
+run =
+    Script.withoutCliOptions
+        (Script.log "Hello from elm-pages Scripts!")
